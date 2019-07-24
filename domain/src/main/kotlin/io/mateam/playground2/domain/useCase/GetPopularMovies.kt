fe3357fbe8f46dbb9e3aeb33509fdfcade0a0949
Oleynik, Andrey (ao052r) <ao052r@intl.att.com>
2019-07-24 12:45:02 +0300

    Update Failure and Param type in GetPopularMovies use case

--- domain/src/main/kotlin/io/mateam/playground2/domain/useCase/GetPopularMovies.kt
+++ domain/src/main/kotlin/io/mateam/playground2/domain/useCase/GetPopularMovies.kt
@@ -2,15 +2,20 @@ package io.mateam.playground2.domain.useCase
 
 import io.mateam.playground2.domain.entity.Failure
 import io.mateam.playground2.domain.entity.PopularMovies
+import io.mateam.playground2.domain.entity.Result
 import io.mateam.playground2.domain.repo.MoviesRepo
 import io.mateam.playground2.domain.utils.Either
-import io.mateam.playground2.domain.entity.Result
 
-class GetPopularMovies(private val repo: MoviesRepo) : BaseUseCase<PopularMovies, Nothing>() {
-    override suspend fun run(params: Nothing): Either<Failure, PopularMovies> {
-        return when (val result = repo.getPopularMovies()){
+class GetPopularMovies(private val repo: MoviesRepo) : BaseUseCase<PopularMovies, GetPopularMovies.None>() {
+
+    override suspend fun run(params: None): Either<GetPopularFailure, PopularMovies> {
+        return when (val result = repo.getPopularMovies()) {
             is Result.Success -> Either.Right(result.data)
-            is Result.Error -> Either.Left(Failure.FeatureFailure(result.exception))
+            is Result.Error -> Either.Left(GetPopularFailure(result.exception))
         }
     }
+
+    class None : Any()
+
+    data class GetPopularFailure(val error: Exception) : Failure.FeatureFailure(error)
 }
